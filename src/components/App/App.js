e66611b002400144293acf122b561427eaedefd9
dave8git@gmail.com <dave8git@gmail.com>
2020-12-14 20:54:52 +0100

    add Wykorzystanie props assy 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,11 +8,8 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world</h2>
-        <List title={["Things to do ", <sup>soon!</sup>]}>
-        </List>
-        <img image={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"}/>
-         
-      
+        <List title={["Things to do ", <sup>soon!</sup>]} image={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"}>
+        </List>    
       </main>
     );
   }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Hero = props => (
       <header className={styles.component}>
         <h2 className={styles.title}>{props.titleText}</h2> 
-        <img src={image}/>
+        <img className={styles.image} src={props.image} />
       </header>
     );
 
